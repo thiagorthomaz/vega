bd98c240f5b97b11ccc7e3c7e7f420bc2d489d68
Thiago <thiagorthomaz@gmail.com>
2019-04-13 18:09:31 -0300

    Created API

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using vega.Persistence;
+using AutoMapper;
 
 namespace vega
 {
@@ -22,6 +23,8 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddAutoMapper();
 
             services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:Default"]));
 
