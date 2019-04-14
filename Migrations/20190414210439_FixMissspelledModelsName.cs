df0fe04547098ac8bb32bf5b86a27ba7896c9eca
Thiago <thiagorthomaz@gmail.com>
2019-04-14 18:05:37 -0300

    Fixed Model name

---
+++ Migrations/20190414210439_FixMissspelledModelsName.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class FixMissspelledModelsName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update Models set Name = 'Make3-ModelB' where Name='Make2-ModelB' and MakeId=3");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
