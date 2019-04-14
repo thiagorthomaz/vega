6a8ee934fc4ee6981b70b0d9e4f9cf4f96156e53
Thiago <thiagorthomaz@gmail.com>
2019-04-14 16:09:49 -0300

    Added Features

---
+++ Migrations/20190414183951_SeedFeatures.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class SeedFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Features (Name) values ('Features1')");
+            migrationBuilder.Sql("insert into Features (Name) values ('Features2')");
+            migrationBuilder.Sql("insert into Features (Name) values ('Features3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Features where Name in ('Features1', 'Features2', 'Features3')");
+        }
+    }
+}
